cdf24ff6b48b120d2fb94bbb134b691bb274ff08
Asak <oseasak@gmail.com>
2023-04-23 14:42:34 -0700

    movement

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "";
 
-	for (nlett = 0; text_content[nletters]; nlett++)
+	for (nlett = 0; text_content[nlett]; nlett++)
 		;
 
 	rw = write(fad, text_content, nlett);
