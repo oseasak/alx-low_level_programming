80ccf41e3eba81ede58c0ededac1be122ca16b36
PateK <107476874+oseasak@users.noreply.github.com>
2023-04-23 22:58:20 +0100

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 /**
- * error_file - checks if files can be opened.
- * @file_from: file_from.
- * @file_to: file_to.
- * @argv: arguments vector.
- * Return: no return.
+ * error_file - check if the files can be opened.
+ * @file_from: file_from
+ * @file_to: file_to
+ * @argv: arguments vector
+ * Return: no return
  */
 void error_file(int file_from, int file_to, char *argv[])
 {
@@ -23,15 +23,15 @@ void error_file(int file_from, int file_to, char *argv[])
 }
 
 /**
- * main - check the code for Holberton School students.
- * @argc: number of arguments.
- * @argv: arguments vector.
- * Return: Always 0.
+ * main - check code for Holberton School students
+ * @argc: number of arguments
+ * @argv: arguments vector
+ * Return: Always 0
  */
 int main(int argc, char *argv[])
 {
 	int file_from, file_to, err_close;
-	ssize_t nchars, nwr;
+	ssize_t ncha, nw;
 	char buf[1024];
 
 	if (argc != 3)
@@ -44,14 +44,14 @@ int main(int argc, char *argv[])
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	error_file(file_from, file_to, argv);
 
-	nchars = 1024;
-	while (nchars == 1024)
+	ncha = 1024;
+	while (ncha == 1024)
 	{
-		nchars = read(file_from, buf, 1024);
-		if (nchars == -1)
+		ncha = read(file_from, buf, 1024);
+		if (ncha == -1)
 			error_file(-1, 0, argv);
-		nwr = write(file_to, buf, nchars);
-		if (nwr == -1)
+		nw = write(file_to, buf, ncha);
+		if (nw == -1)
 			error_file(0, -1, argv);
 	}
 
