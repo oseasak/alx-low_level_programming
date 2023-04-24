8f3d64a22ffb99bffef03422bc599a4423e014ba
PateK <107476874+oseasak@users.noreply.github.com>
2023-04-24 22:11:54 +0100

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a linked list
- * @h: linked list of type listint_t to print
+ * print_listint - prints elements of a linked list
+ * @h: linked list type listint_t to print
  *
- * Return: number of nodes
+ * Return: number nodes
  */
 size_t print_listint(const listint_t *c)
 {
@@ -18,3 +18,4 @@ size_t print_listint(const listint_t *c)
     }
 
     return (num);
+}
